8df450a7268024e273cdb0e301dc6115c029e763
Milh Hdz <neo.dev@hotmail.com>
2022-11-01 23:23:04 -0600

    settings to the project's essential bases.

--- yelp/scripts/insert_default_data.py
+++ yelp/scripts/insert_default_data.py
@@ -1,140 +1,110 @@
 import json
-import os
+import time
+from django_bulk_load import bulk_insert_models
 
-# from ..models import YelpUser
+
 from yelp.models.business import Business
 from yelp.models.users import YelpUser
 from yelp.models.reviews import Review
 
-# def run():
-#     with open(f"{os.getcwd()}/data/user100k.json", "r") as reader:
-#         while True:
-#             line = reader.readline()
-#             if not line:
-#                 break
-#             data = json.loads(line)
-#             YelpUser.objects.create(**data)
+def insert_data_business():
+    with open("/home/yelp/yelp/scripts/data/business10k.json", "r") as reader:
+        list_business = []
+        for idx, line in enumerate(reader):
+            if not line:
+                break
+            data = json.loads(line)
+            if "categories" in data and data["categories"] != None:
+                print(idx)
+                print(data["categories"])
+                data["categories"] = data["categories"].split(",")
 
-# def run():
-#     data_info = [
-#         ("business10k.json", Business),
-#         # ("user100k.json", YelpUser),
-#         # ("review1M.json", Review),
-#     ]
+            if "friends" in data and data["friends"] != None:
+                data["friends"] = data["friends"].split(",")
 
-#     for file_name, class_model in data_info:
-#         with open(f"/home/yelp/yelp/scripts/data/{file_name}", "r") as reader:
-#             for idx, line in enumerate(reader):
-#                 if not line:
-#                     break
-#                 data = json.loads(line)
-#                 if "categories" in data and data["categories"] != None:
-#                     print(idx)
-#                     print(data["categories"])
-#                     data["categories"] = data["categories"].split(",")
-
-#                 if "friends" in data and data["friends"] != None:
-#                     data["friends"] = data["friends"].split(",")
-
-#                 if "elite" in data and data["elite"] != None:
-#                     data["elite"] = data["elite"].split(",")
+            if "elite" in data and data["elite"] != None:
+                data["elite"] = data["elite"].split(",")
 
 
+            list_business.append( Business(**data) )
+        
+        inicio = 0
+        fin = 5000
+        while(fin <= 10000):
+            time_inicio = time.time()
 
+            new_list = list_business[inicio:fin]
+            bulk_insert_models(new_list)
+            print(f"----\n{fin}\n----")
+            inicio = fin
+            fin += 5000
+            time.sleep(2)
 
-#                 class_model.objects.create(**data)
+            print(f"----\n{time.time() - time_inicio}\n----")
 
+def insert_data_users():    
+    with open("/home/yelp/yelp/scripts/data/user100k.json", "r") as reader:
+        list_users = []
+        for idx, line in enumerate(reader):
+            if not line:
+                break
+            data = json.loads(line)
 
+            list_users.append( YelpUser(**data) )
 
-import time
-from io import StringIO
-import pandas as pd
+        inicio = 0
+        fin = 5000
+        while(fin <= 100000):
+            time_inicio = time.time()
 
-from contextlib import closing
+            new_list = list_users[inicio:fin]
+            bulk_insert_models(new_list)
+            print(f"----\n{fin}\n----")
+            inicio = fin
+            fin += 5000
+            print(f"----\n{time.time() - time_inicio}\n----")
+            time.sleep(2)
 
-from django_bulk_load import bulk_insert_models
+def insert_data_reviews():
+    with open("/home/yelp/yelp/scripts/data/review1M.json", "r") as reader:
+        list_users = []
+        for idx, line in enumerate(reader):
+            if idx == 100000:
+                break
+            data = json.loads(line)
 
-def in_memory_csv(data):
-    """Creates an in-memory csv.
+            try:
+                user = YelpUser.objects.only("user_id").get(user_id=data["user_id"])
+                data["user_id"] = user
+            except YelpUser.DoesNotExist:
+                data["user_id"] = None
+            try:
+                business = Business.objects.only("business_id").get(business_id=data["business_id"])
+                data["business_id"] = business
+            except Business.DoesNotExist:
+                data["business_id"] = None
 
-    Assumes `data` is a list of dicts
-    with native python types."""
+            list_users.append( Review(**data) )
 
-    mem_csv = StringIO()
-    pd.DataFrame(data).to_csv(mem_csv, index=False)
-    mem_csv.seek(0)
-    return mem_csv
+        inicio = 0
+        fin = 5000
+        while(fin <= 100000):
+            time_inicio = time.time()
 
-# def run():
-#     data_info = [
-#         # ("business10k.json", Business),
-#         ("user100k.json", YelpUser),
-#         # ("review1M.json", Review),
-#     ]
+            new_list = list_users[inicio:fin]
+            bulk_insert_models(new_list)
+            print(f"----\n{fin}\n----")
+            inicio = fin
+            fin += 5000
+            time.sleep(2)
 
-#     for file_name, class_model in data_info:
-#         with open(f"/home/yelp/yelp/scripts/data/{file_name}", "r") as reader:
-#             list_users = []
-#             for idx, line in enumerate(reader):
-#                 if not line:
-#                     break
-#                 data = json.loads(line)
-
-#                 list_users.append( YelpUser(**data) )
-#             print("----")
-
-#             inicio = 0
-#             fin = 1000
-#             while(fin <= 100000):
-#                 new_list = list_users[inicio:fin]
-#                 bulk_insert_models(new_list)
-#                 time.sleep(2)
-#                 print(f"----\n{fin}\n----")
-#                 inicio = fin
-#                 fin += 1000
+            print(f"----\n{time.time() - time_inicio}\n----")
 
 
 def run():
-    data_info = [
-        # ("business10k.json", Business),
-        # ("user100k.json", YelpUser),
-        ("review1M.json", Review),
-    ]
-
-    for file_name, class_model in data_info:
-        with open(f"/home/yelp/yelp/scripts/data/{file_name}", "r") as reader:
-            list_users = []
-            for idx, line in enumerate(reader):
-                if idx == 100000:
-                    break
-                data = json.loads(line)
-
-                try:
-                    user = YelpUser.objects.only("user_id").get(user_id=data["user_id"])
-                    data["user_id"] = user
-                except YelpUser.DoesNotExist:
-                    data["user_id"] = None
-                try:
-                    business = Business.objects.only("business_id").get(business_id=data["business_id"])
-                    data["business_id"] = business
-                except Business.DoesNotExist:
-                    data["business_id"] = None
-
-                list_users.append( Review(**data) )
-
-            inicio = 0
-            fin = 5000
-            while(fin <= 100000):
-                time_inicio = time.time()
-
-                new_list = list_users[inicio:fin]
-                bulk_insert_models(new_list)
-                time.sleep(2)
-                print(f"----\n{fin}\n----")
-                inicio = fin
-                fin += 5000
-
-                print(f"----\n{time.time() - time_inicio}\n----")
-            
-
-                
+    print("---BUSINESS---")
+    insert_data_business()
+    print("---USERS---")
+    insert_data_users()
+    print("---REVIEWS---")
+    insert_data_reviews()
